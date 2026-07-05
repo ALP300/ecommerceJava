143f1acc9d7f03e119710d7aec2d24fb779812a0
ALP300 <aitorpalomino1@gmail.com>
2026-07-04 19:26:37 -0500

    a

--- src/main/java/com/ecommerce/ecommerceJava/controller/ProductController.java
+++ src/main/java/com/ecommerce/ecommerceJava/controller/ProductController.java
@@ -17,28 +17,28 @@ import io.swagger.v3.oas.annotations.parameters.RequestBody;
 @RestController
 @RequestMapping("api/products")
 public class ProductController {
-    
+
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-    
+
     @GetMapping
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
-    
+
     @GetMapping("/{id}")
     public Product getProductById(@PathVariable Long id) {
         return productService.getProductById(id);
     }
-    
+
     @PostMapping
     public Product createProduct(@RequestBody Product product) {
         return productService.createProduct(product);
     }
-    
+
     @DeleteMapping("/{id}")
     public void deleteProduct(@PathVariable Long id) {
         productService.deleteProduct(id);
